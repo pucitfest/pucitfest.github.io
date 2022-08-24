cbded3f5605bbb60625adf6404dbb092328b212b
pucitfest <112024920+pucitfest@users.noreply.github.com>
2022-08-25 02:42:33 +0600

    init

---
+++ src/components/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="bg-gray-300 ">
+    <div class="min-h-screen flex items-top justify-center">
+      <div class="relative w-full max-w-full">
+        <div class="fixed top-0 -left-4 w-96 h-72 bg-purple-400 rounded-full mix-blend-multiply filter blur-2xl opacity-70"></div>
+        <div class="fixed top-8 left-24 w-72 h-72 bg-pink-400 rounded-full mix-blend-multiply filter blur-2xl opacity-70"></div>
+        <div class="fixed top-0 -right-4 w-72 h-72 bg-purple-400 rounded-full mix-blend-multiply filter blur-2xl opacity-70"></div>
+        <div class="fixed top-8 right-24 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70"></div>
+        
+        <div class="fixed top-12 -left-36 w-96 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70"></div>
+        <div class="fixed top-20 left-48 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70"></div>
+        <div class="fixed top-12 -right-36 w-72 h-72 bg-purple-400 rounded-full mix-blend-multiply filter blur-2xl opacity-70"></div>
+        <div class="fixed top-20 right-48 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-2xl opacity-70"></div>
+        <!-- <router-view/>  -->
+        <div>
+          <div class="text-center shadow-xl mt-2 bg-white opacity-50 rounded-xl p-4 w-full 2xl:max-w-screen-2xl xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md max-w-sm mx-auto">
+            <!-- <h2 class="font-semibold text-2xl text-gray-900 p-3">PUC IT FEST 2022</h2> -->
+            <div class="object-none object-center w-72 h-72 mb-1 shadow-lg rounded-xl text-white  mx-auto sticky max-w-screen-sm p-1.5 bg-gradient-to-r from-purple-600 to-pink-700 cursor-pointer opacity-70" type="submit">
+              <img class="rounded-lg" src="../assets/logo.jpg" alt="" srcset="" width="450px" height="450px">
+            </div>
+          </div>
+          <div class="text-center shadow-xl mt-2 bg-white opacity-70 rounded-xl p-4 w-full 2xl:max-w-screen-2xl xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md max-w-sm mx-auto">
+            <div class="w-6/12 p-2 xl:w-full 2xl:w-full lg:w-full md:w-full sm:w-full shadow-3xl">
+              <div class="bg-gray-200 animate-pulse h-full rounded-md p-2 flex space-x-4">
+                <div class="object-none object-center mb-1 shadow-lg rounded-xl text-white  mx-auto sticky max-w-screen-sm p-1.5 bg-gradient-to-r from-purple-600 to-pink-700 cursor-pointer opacity-70" type="submit">
+                  <img class="rounded-lg" src="../assets/comingsoon.jpg" alt="" srcset="" width="450px" height="450px">
+                </div>
+              </div>                    
+            </div>                    
+          </div>
+          <div class="text-center shadow-xl mt-2 bg-white opacity-50 rounded-xl p-4 w-full 2xl:max-w-screen-2xl xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md max-w-sm mx-auto">
+            <div class="w-6/12 p-2 xl:w-full 2xl:w-full lg:w-full md:w-full sm:w-full opacity-60 shadow-3xl">
+              <div class="animate-pulse bg-gray-300 h-full rounded-md p-2 flex space-x-4">
+                <div class="flex-1 space-y-4 py-1">
+                  <div class="h-4 bg-gray-400 dark:bg-red-400 rounded w-3/4"></div>
+                  <div class="space-y-2">
+                    <div class="h-4 bg-gray-400 dark:bg-red-400 rounded"></div>
+                    <div class="h-4 bg-gray-400 dark:bg-red-400 rounded w-5/6"></div>
+                  </div>
+                </div>
+              </div>                    
+            </div>                    
+          </div>
+        </div>
+      </div>
+    </div>
+  <footer class="p-2 w-full max-w-md mx-auto">
+    <div class="text-gray-500 text-center font-thin font-sans text-sm"> <small> Developed By </small><a href="https://github.com/mohsin-riad" class=" font-semibold text-gray-400 "> Mohsin Riad </a> </div>
+  </footer>
+  </div>
+</template>
