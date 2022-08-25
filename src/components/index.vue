ec48002ce5e1f563bfae6658ded1abbf59faff67
pucitfest <112024920+pucitfest@users.noreply.github.com>
2022-08-25 12:23:31 +0600

    release

--- src/components/index.vue
+++ src/components/index.vue
@@ -14,19 +14,17 @@
         <!-- <router-view/>  -->
         <div>
           <div class="text-center shadow-xl mt-2 bg-white opacity-50 rounded-xl p-4 w-full 2xl:max-w-screen-2xl xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md max-w-sm mx-auto">
-            <!-- <h2 class="font-semibold text-2xl text-gray-900 p-3">PUC IT FEST 2022</h2> -->
-            <div class="object-none object-center w-72 h-72 mb-1 shadow-lg rounded-xl text-white  mx-auto sticky max-w-screen-sm p-1.5 bg-gradient-to-r from-purple-600 to-pink-700 cursor-pointer opacity-70" type="submit">
+            <h2 class="mb-3 text-4xl font-extrabold text-white p-3 bg-gradient-to-r from-purple-600 to-pink-700 rounded-lg opacity-70">PUC IT FEST 2022</h2>
+            <div class="object-center w-72 h-72 mb-1 shadow-lg rounded-xl text-white  mx-auto sticky max-w-screen-sm p-1.5 bg-gradient-to-r from-purple-600 to-pink-700 cursor-pointer opacity-70" type="submit">
               <img class="rounded-lg" src="../assets/logo.jpg" alt="" srcset="" width="450px" height="450px">
             </div>
 
           </div>
           <div class="text-center shadow-xl mt-2 bg-white opacity-70 rounded-xl p-4 w-full 2xl:max-w-screen-2xl xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md max-w-sm mx-auto">
-            <div class="p-2 2xl:max-w-screen-2xl xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md max-w-sm mx-auto opacity-60 shadow-3xl">
-              <div class="bg-gray-200 animate-pulse h-full rounded-md p-2 flex space-x-4">
-                <div class="object-none object-center mb-1 shadow-lg rounded-xl text-white  mx-auto sticky max-w-screen-sm p-1.5 bg-gradient-to-r from-purple-600 to-pink-700 cursor-pointer opacity-70" type="submit">
-                  <img class="rounded-lg" src="../assets/comingsoon.jpg" alt="" srcset="" width="450px" height="450px">
-                </div>
-              </div>                    
+            <div class="bg-gray-200 h-full rounded-md flex space-x-4 p-2 2xl:max-w-screen-2xl xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md max-w-sm mx-auto opacity-60 shadow-3xl">
+              <div class="object-none object-center mb-1 shadow-lg rounded-xl text-white  mx-auto sticky max-w-screen-sm p-1.5 bg-gradient-to-r from-purple-600 to-pink-700 cursor-pointer opacity-70" type="submit">
+                <img class=" animate-pulse rounded-lg" src="../assets/comingsoon.jpg" alt="" srcset="" width="450px" height="450px">
+              </div>
             </div>                    
           </div>
           <div class="text-center shadow-xl mt-2 bg-white opacity-50 rounded-xl p-4 w-full 2xl:max-w-screen-2xl xl:max-w-screen-xl lg:max-w-screen-lg md:max-w-screen-md max-w-sm mx-auto">
@@ -46,7 +44,7 @@
       </div>
     </div>
   <footer class="p-2 w-full max-w-md mx-auto">
-    <div class="text-gray-500 text-center font-thin font-sans text-sm"> <small> Developed By </small><a href="https://github.com/mohsin-riad" class=" font-semibold text-gray-400 "> Mohsin Riad </a> </div>
+    <div class="text-gray-500 text-center font-thin font-sans text-sm"> <small> Developed By </small><a href="http://cse.puc.ac.bd/" class=" font-semibold text-gray-400 "> Premier University Computer Club </a> </div>
   </footer>
   </div>
 </template>
